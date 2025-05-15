0ce789293c00eb3186cc36dd91a5501c651ba89e
Alessandro Fernandes Antunes <alesfernandes123@gmail.com>
2025-05-15 00:12:50 +0000

    lol

---
+++ ex4/Program.cs
@@ -0,0 +1,29 @@
+﻿ public class Branco{
+ public static void Main () {
+    double vagneris = Negao();
+    Console.WriteLine("" + vagneris);
+ }
+
+ public static double Negao (){
+double maior_numero = 434;
+double menor_numero =152;
+
+double resto = maior_numero % menor_numero;
+while (resto !=0)
+        {
+            menor_numero = menor_numero % resto;
+            
+            if (menor_numero < resto)
+            {
+                maior_numero = resto;
+                resto = menor_numero;
+                menor_numero = maior_numero;
+
+            }
+
+            
+        }
+        return menor_numero;
+
+ }
+ }
